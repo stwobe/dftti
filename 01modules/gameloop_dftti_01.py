95d80635649035639958e5f27a04e56cee715938
strobez2017 <cwmbranmathstutor@gmail.com>
2017-01-09 20:37:13 +0000

    Initial set up:
    	Add first pseudo-python code for the game loop

---
+++ 01modules/gameloop_dftti_01.py
@@ -0,0 +1,109 @@
+#! /usr/bin/python
+# Scratch original - DFTTI - https://scratch.mit.edu/projects/53667120/
+
+# set location of all sprites when gameplay starts
+def location_of_initial_sprites_positions():
+    def background_sprites(sky, ice, sun, trees):
+    def movable_sprites(sally, stomper, log, poo):
+
+def music_track():
+    # tunes here
+
+def gameloop():
+
+    # move sally
+    def has_sallys_movement_keys_been_pressed(left, right):
+    def has_sallys_tracks_key_been_pressed(down):
+    def which_key():
+    def change_her_position():
+    def change_costume():
+
+    # move stomper
+    def has_stompers_movement_keys_been_pressed(left, right):
+    def has_stompers_stomping_key_been_pressed(down):
+    def which_key():
+    def change_his_position():
+    def change_costume():
+
+    # move poo
+
+    # move log
+
+    # poo collision with Sally's tracks and Stomper's ice cracks
+
+    # log collision with Sally's tracks and Stomper's ice cracks
+
+    #sally collision
+    def has_sally_touched_green():
+    def has_sally_touched_cracked_ice_3():
+    def has_sally_touched_log_or_poo():
+    def has_sally_touched_edge_of_screen():
+
+    #stomper collision
+    def has_stomper_touched_green():
+    def has_stomper_touched_sally_tracks():
+    def has_stomper_touched_log_or_poo():
+    def has_stomper_touched_edge_of_screen():
+
+    # log collision with edge of screen
+
+    # poo collision with edge of screen
+
+
+def draw_screen():
+    # draw sprites in new positions
+    def draw_sally_new_position:
+    def draw_stomper_new_position():
+    def draw_poo_new_position():
+    def draw_log_new_position():
+
+    def draw cracked_ice_1():
+    def draw cracked_ice_2():
+    def draw cracked_ice_3():
+    def draw_sally_trail()::
+
+def sounds():
+    music
+    sally_skating
+    stomper_skating
+    stomper_stomping
+
+
+# end game section
+    # if Sally has touched cracked_ice_3 then Stomper wins
+    # if Stomper has touched Sally's tracks then Sally wins'
+
+'''
+to_do():
+    look_up_pickle
+    finish_pseudocode
+    convert_to_pygame
+
+sprite_list():
+    sally_1
+    sally_2
+    sally_3
+    stomper_1
+    stomper_2
+    stomper_3
+    cracked_ice_1
+    cracked_ice_2
+    cracked_ice_3
+    floating_poo
+    floating_log
+    sally_tracks
+
+art_list():
+	frozen lake
+	trees
+	grass
+	sky
+	sun
+
+sound_list():
+    backing_track_loop
+    sally_skate_sound_effect
+    stomping_sound_effect
+    stomper_skate_sound_effect
+
+'''
